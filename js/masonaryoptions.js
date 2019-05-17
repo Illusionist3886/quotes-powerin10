a6fe5ca97bdc16c646d5d442c5629f102c4df523
Delowar Hossain <contact.delowar@gmail.com>
2019-05-17 12:18:16 +0600

    Carouse Added | Overlay Fixed | Tags New Style

--- js/masonaryoptions.js
+++ js/masonaryoptions.js
@@ -1,6 +1,41 @@
-$('.grid').masonry({
-    columnWidth: 20,
-    itemSelector: '.grid-item',
-    gutter: 26,
-    horizontalOrder: true
-});
+jQuery(window).on('load', function() {
+    $('.grid').masonry({
+        columnWidth: 20,
+        itemSelector: '.grid-item',
+        gutter: 26,
+    });
+
+    
+  });
+
+  $(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        // autoPlay:true,
+        // autoplayTimeout:2000,
+        // autoplayHoverPause:true,
+        margin: 15,
+        navText: ['<i class="fas fa-arrow-circle-left fa-2x text-info""></i>','<i class="fas fa-arrow-circle-right fa-2x text-info""></i>'],
+        dots: false,
+        center: true,
+
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:1,
+                nav:false
+            },
+            1000:{
+                items:1,
+                nav:true,
+            }
+        },
+    });
+  });
+
+
+  
